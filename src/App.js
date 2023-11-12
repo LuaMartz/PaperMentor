74e366da35dc35d267d221ed6136159335d9aa49
LuaMartz <luisamartinez030503@gmail.com>
2023-11-11 20:53:10 -0500

    formatting app skeleton

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import onboarding from './components/onboarding';
+import './styles/main.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <onboarding />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/onboarding.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './onboarding.css';
+
+const onboarding = () => {
+  return (
+    <div className="onboarding">
+        
+    </div>
+  );
+};
+
+export default onboarding;
